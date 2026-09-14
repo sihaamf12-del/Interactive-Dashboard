3aaf1ebd5fd048def09f17d6e0e37c64c028e4de
sihaamf12-del <sihaamf12@gmail.com>
2026-09-14 19:28:24 -0400

    Added a few comments explaining the code

--- js/task-manager.js
+++ js/task-manager.js
@@ -2,6 +2,8 @@
 // Uses the original calculation in debug.html
 function weeklyGoal(userName, dailyGoal, bonusTasks) {
 
+    console.log("Checking status for: " + userName);
+    
     let weeklyTotal = dailyGoal * 5;
     let totalGoal = weeklyTotal + bonusTasks;
     let output = "User: " + userName + "<br>Total Weekly Goal: " + totalGoal;
@@ -11,16 +13,20 @@ function weeklyGoal(userName, dailyGoal, bonusTasks) {
 }
 
 // Event listener for whenever the user clicks the submit button
+// Instead of making a seperate variable for goal-btn I just put it directly here but it would be the same either way
 document.getElementById("goal-btn").addEventListener("click", function (event) {
 
+    // Prevents the page from reloading like it would normally do, hence the "Default"
     event.preventDefault();
 
     // Allows the three values to be inputted by the user and stored in variables rather than decided like in debug.html
     let userName = document.getElementById("user-name").value;
+    
+    // Convert string to number
     let dailyGoal = Number(document.getElementById("daily-goal").value);
     let bonusTasks = Number(document.getElementById("bonus-tasks").value);
 
-    console.log("Checking status for: " + userName);
+    // Prints everything out using the weeklyGoal function above
     weeklyGoal(userName, dailyGoal, bonusTasks);
 
 });
