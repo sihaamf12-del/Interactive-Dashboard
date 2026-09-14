b8006a955e7b5380127fbe84da845644fb169b15
Sihaam Farah <sihaamf12@gmail.com>
2026-09-14 18:59:25 -0400

    git commit -m "Refactor task manager code and update styles"

---
+++ js/task-manager.js
@@ -0,0 +1,26 @@
+// Function calculates the weekly goal based on user input and displays it in the HTML element with id "goal-message"
+// Uses the original calculation in debug.html
+function weeklyGoal(userName, dailyGoal, bonusTasks) {
+
+    let weeklyTotal = dailyGoal * 5;
+    let totalGoal = weeklyTotal + bonusTasks;
+    let output = "User: " + userName + "<br>Total Weekly Goal: " + totalGoal;
+
+    document.getElementById("goal-message").innerHTML = output;
+
+}
+
+// Event listener for whenever the user clicks the submit button
+document.getElementById("goal-btn").addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    // Allows the three values to be inputted by the user and stored in variables rather than decided like in debug.html
+    let userName = document.getElementById("user-name").value;
+    let dailyGoal = Number(document.getElementById("daily-goal").value);
+    let bonusTasks = Number(document.getElementById("bonus-tasks").value);
+
+    console.log("Checking status for: " + userName);
+    weeklyGoal(userName, dailyGoal, bonusTasks);
+
+});
